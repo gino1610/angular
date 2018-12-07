1c989b409e8a8c5234fd5fd8bb507eeff253ff56
Srinivas <gino1610@gmail.com>
2018-12-06 20:33:22 -0800

    Tour-of-Heroes: displaying heroes list and updating styles

--- quickstart/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ quickstart/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +11,17 @@ import {Hero} from '../hero';
 
 export class HeroesComponent implements OnInit {
 
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+    // this.selectedHero = HEROES[0];
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
